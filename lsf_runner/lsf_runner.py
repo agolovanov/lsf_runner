af637001bc77f6233f38eb10f209597ecf8a2f13
Anton Golovanov <agolovanov256@gmail.com>
2023-07-26 14:49:45 +0300

    Move code to a separate file

---
+++ lsf_runner/lsf_runner.py
@@ -0,0 +1,35 @@
+def run_job(command, job_number, job_name=None, queue=None, use_gpu=False, rerunnable=False):
+    """Run an LSF job
+
+    Parameters
+    ----------
+    command : str
+        the command to run
+    job_number : int
+        number of jobs
+    job_name : str, optional
+        job name, by default None
+    queue : str, optional
+        job queue to submit to, by default None
+    use_gpu : bool, optional
+        request GPU, by default False
+    rerunnable : bool, optional
+        make the program rerunnable or non-rerunnable (-rn flag), by default False
+    """
+    import subprocess
+
+    if job_name is None:
+        job_name = 'job'
+    
+    bsub_arguments = ['-J', job_name, '-o', f'logs/{job_name}-%J.out', '-n', str(job_number)]
+    if queue is not None:
+        bsub_arguments += ['-q', queue]
+    if use_gpu:
+        bsub_arguments += ['-gpu', '-']
+    if not rerunnable:
+        bsub_arguments += ['-rn']
+    
+
+    lsf_command = ['bsub'] + bsub_arguments + [command] 
+    print(f'Running: {" ".join(lsf_command)}')
+    subprocess.run(lsf_command)
